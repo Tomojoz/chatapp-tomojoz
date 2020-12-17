9a5dfce2d5af49363c794545b8a447ffb624d60e
tomo_joz <tomoya.shozu@gmail.com>
2020-12-17 15:16:45 +0900

    新しいボードを作成できるようにしました。

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -2,4 +2,28 @@ class BoardsController < ApplicationController
     def index
         @boards = Board.all
     end
+
+    def show
+    end
+
+    def new
+        @board = current_user.boards.build
+    end
+
+    def create
+        @board = current_user.boards.build(board_params)
+        if @board.save
+            redirect_to board_path(@board), notice: '保存しました'
+        else
+            flash.now[:error] = '保存できませんでした'
+            render :new
+        end
+    end
+
+    private
+    def board_params
+
+        params.require(:board).permit(:title, :description)
+
+    end
 end
