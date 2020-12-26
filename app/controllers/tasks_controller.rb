4aead30def739e8e92ca46ebfe59c573d0248fdb
tomo_joz <tomoya.shozu@gmail.com>
2020-12-26 09:20:03 +0900

    task の new を修正しました。

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,8 +7,8 @@ class TasksController < ApplicationController
     end
 
     def new
-        board = Board.find(params[:board_id])
-        @task = board.tasks.build
+        @board = Board.find(params[:board_id])
+        @task = @board.tasks.build
     end
 
     def create
